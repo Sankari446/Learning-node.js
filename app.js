2a4383b3741f45fef7160fa5d81ee2c1e2edc7c7
Sankari446 <sank17ec119@rmkcet.ac.in>
2021-02-27 20:26:08 +0530

    cookies

--- app.js
+++ app.js
@@ -1,34 +1,41 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const sankRoutes = require('./routes/sankRoutes');
-const { success, error } = require("consola");
+const cookieParser = require('cookie-parser');
+
 const app = express();
 
 // middleware
 app.use(express.static('public'));
 app.use(express.json());
+app.use(cookieParser());
 
 // view engine
 app.set('view engine', 'ejs');
 
 // database connection
 const dbURI = 'mongodb+srv://sank123:F7qEZLZy1IcsTksY@cluster0.v4kn9.mongodb.net/node-sank';
-mongoose
-  .connect(dbURI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-  })
-  .then((result) => success({ message: `MONGO DB CONNECTED`, badge: true }))
-  .catch((err) =>
-    error({ message: `UNABLE TO CONNECT TO DATABASE ${err}`, badge: true })
-  );
+mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+  .then((result) =>  app.listen(3000))
+  .catch((err) => console.log(err));
 
 // routes
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', (req, res) => res.render('smoothies'));
 app.use(sankRoutes);
 
-app.listen(3000, () => {
-  success({ message: `SERVER STARTED ON PORT 3000`, badge: true });
-})
+//cookies
+app.get('/set-cookies', (req, res) => {
+
+  //res.setHeader('Set-Cookie', 'newUser=true');
+
+  res.cookie('newUser', false);
+  //res.cookie('isEmployee', true, { maxAge: 1000 * 60 * 60 * 24, secure: true });
+
+  res.send('you got the cookies!');
+});
+
+app.get('/read-cookies', (req, res) => {
+
+});
+
