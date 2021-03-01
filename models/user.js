55cf6c181e9387488851b8fc7ebe663133deccc3
Sankari446 <sank17ec119@rmkcet.ac.in>
2021-03-01 12:18:48 +0530

    logging users1

--- models/user.js
+++ models/user.js
@@ -24,6 +24,19 @@ userSchema.pre('save', async function(next) {
      next();
 });
 
+// static method to login user
+userSchema.statics.login = async function(email, password) {
+    const user = await this.findOne({ email });
+    if (user) {
+       const sank = await bcrypt.compare(password, user.password);
+       if (sank) {
+           return user;
+       }
+       throw Error('incorrect password');
+    }
+    throw Error('incorrect email');
+}
+
 const User = mongoose.model('user',userSchema);
 
 module.exports = User;
